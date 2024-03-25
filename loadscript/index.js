a63c61185dc52c43e39467d8172cd2a8e5458dbf
Camel Djoulako <cameldjoulako17@gmail.com>
2024-03-25 11:01:44 +0100

    exemple utilisation promesse

--- loadscript/index.js
+++ loadscript/index.js
@@ -1,105 +1,36 @@
-/* let promise = new Promise(function (resolve, reject) {
-  //l'exécuteur
-});
-/* 
-etat (state): pending, fulfilled, 
-result: undifined se change en value resolve(value) : rejet(error)
+//Méthode classique
 
+/* function loadScript(src, callback) {
+  let script = document.createElement("script");
 
-*/
+  script.src = src;
 
-// resolve(value)
+  script.onload = () => callback(null, script);
+  script.onerror = () =>
+    callback(new Error(`Erreur de chargement du fichier: ${src}`));
 
-//rejet(error) */
+  document.head.append(script);
+} */
 
-/* function bonjour() {
-    console.log("bonjour");
- } */
+///Utilisation des promesse
 
-/* let promise = new Promise(function (resolve, reject) {
-//traitement
+function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    let script = document.createElement("script");
+    script.src = src;
 
-  setTimeout(() => resolve("done"), 1000);
-});
+    script.onload = () => resolve(script);
 
-console.log(promise); */
+    script.onerror = () =>
+      reject(new Error(`Erreur de chargement du fichier: ${src}`));
 
-//executeur qui rompe la promesse
+    document.head.append(script);
+  });
+}
+const URL = "https://aazecdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js";
+let promise = loadScript(URL);
 
-/* let promise = new Promise(function (resolve, rejec+t) {
-
-    //traitement ok
-
-  //signlalé apres 1seconde que la tache est terminé avec une erreur
-  setTimeout(() => reject(new Error("une erreur a été produite")), 1000);
-});
-
-console.log(promise); */
-
-//rejet et resolve
-/* let promise = new Promise(function (resolve, reject) {
-  //traitement ok
-  resolve("done");
-
-  //traitrement off
-  reject(new Error("errreur 1..."));
-
-  //signlalé apres 1seconde que la tache est terminé avec une erreur
-  setTimeout(() => reject(new Error("une erreur a été produite")), 1000);
-});
-
-console.log(promise); */
-
-/** CONSOMMATION D'UNE PROMESSE: .then, .catch,  */
-
-/* promise.then(
-  function (result) {
-    // gère un resultat correct
-  },
-  function (error) {
-    // gère une erreur
-  }
+promise.then(
+  (script) => alert(`Le script: ${script.src} a été bien chargé`),
+  (error) => alert(`Erreur: ${error.message}`)
 );
- */
-
-/* let promise = new Promise(function (resolve, reject) {
-  setTimeout(() => resolve("Donne"), 1000);
-}); */
-
-//cas du reject
-/* let promise = new Promise(function (resolve, reject) {
-  setTimeout(() => reject(new Error("une erreur e'est produite")), 1000);
-}); */
-
-/* promise.then(
-  (result) => alert(result),
-  (error) => alert(error)
-); */
-
-//promise.then(null, fonctionGerantErreur)
-
-/* promise.catch(alert);
-
-//FINALLY
-promise.finally() */
-/* 
-new Promise(function (resolve, reject) {
-  //setTimeout(() => reject(new Error("une erreur e'est produite")), 1000);
-}).finally(() => "fin du chargement")
-.then(result => "result ok") */
-
-/* new Promise(function (resolve, reject) {
-  setTimeout(() => resolve("value"), 2000);
-})
-  .finally(() => alert("La promesse est prete"))
-  .then((result) => alert(result));
- */
-
-/* let promise = new Promise((resolve, reject) => {
-  throw new Error("Erreur");
-});
-
-promise
-  .finally(() => alert("la promesse est prête"))
-  .catch((err) => alert(err));
- */
